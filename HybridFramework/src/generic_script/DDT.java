2f68306f8ac391517ae91bb677368e94dddbe9ef
Chandana Gowda <Chandana Gowda@LAPTOP-LFDFSPGC>
2023-07-04 18:47:22 +0530

    BUILD B01

---
+++ HybridFramework/src/generic_script/DDT.java
@@ -0,0 +1,34 @@
+package generic_script;
+
+import java.io.FileInputStream;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class DDT 
+{
+	public static String getData(String sh,int r, int c)
+	{
+		String Value=null;
+		try
+		{
+	
+	FileInputStream fis=new FileInputStream("./Excel/Test.xlsx");
+	Workbook book=WorkbookFactory.create(fis);
+	Sheet sheet=book.getSheet(sh);
+	 Row row=sheet.getRow(r);
+	 Cell cell=row.getCell(c);
+	 Value=cell.toString();
+		}
+	 catch (Exception e)
+	 {
+		System.out.println(e);
+		}
+return Value;
+		}
+}
+
+ 
---
+++ HybridFramework/src/generic_script/Framework_const.java
@@ -0,0 +1,13 @@
+package generic_script;
+
+public interface Framework_const 
+{
+	String chrome_key="webdriver.chrome.driver";
+	String chrome_value="./drives/chromedriver.exe";
+	String gecko_key="webdriver.chrome.driver";
+	String gecko_value="./drives/chromedriver.exe";
+	String base_url="https://wwww.facebook.com";
+	
+	
+
+}
